63cfffcc315e155689d15cee230c65d464541407
Евгений Момотов <jn8momotov@gmail.com>
2018-09-20 14:53:31 +0300

    Initial Commit

---
+++ HookahPlacesMoscow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HookahPlacesMoscow
+//
+//  Created by Евгений on 20.09.2018.
+//  Copyright © 2018 Евгений. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
